e7317ae78bb1a6af367dcc20ed5b08afe7671f14
vibha1081 <113466071+vibha1081@users.noreply.github.com>
2023-01-21 14:34:43 +0530

    day3 vibha

--- bookShelv.js
+++ bookShelv.js
@@ -2,30 +2,46 @@
 let productPage = document.querySelector(".shelvProduct");
 
 // Product data to access golbally
-let globalData=[];
+let globalData = [];
 
 // fetching product data
 fetch("bookShelv.json")
-.then((res)=>res.json())
-.then((data)=>{
-    globalData = data;
-    renderProduct(data);
-}).catch((err)=>console.log(err));
+    .then((res) => res.json())
+    .then((data) => {
+        globalData = data;
+        renderProduct(data);
+    }).catch((err) => console.log(err));
 
 // Rendering product data
-function renderProduct(data){
-      let cardList=`
+function renderProduct(data) {
+    let cartArr = JSON.parse(localStorage.getItem('cart')) || []
+    document.getElementById('count').innerText = cartArr.length;
+    let cardList = `
           <div class="cardList">
-           ${data.map((item)=>productCard(item.image1,item.title,item.description1,item.price,item.rating[0],item.rating[1])).join('')}
+           ${data.map((item) => productCard(item.id, item.image1, item.title, item.description1, item.price, item.rating[0], item.rating[1])).join('')}
           </div>
       `
-      productPage.innerHTML=cardList;
+    productPage.innerHTML = cardList;
+
+    let allProducts = document.getElementsByClassName('productCard');
+
+    for (let item of allProducts) {
+        item.addEventListener('click', (e) => {
+            productClickFn(e)
+        })
+    }
+}
+// product click
+function productClickFn(e) {
+    let id = e.currentTarget.dataset.id;
+    localStorage.setItem('productDetailId', id);
+    document.location.href = "details.html"
 }
 
 // making of each cards
-function productCard(image,title,des,price,rating,rCount){
+function productCard(id, image, title, des, price, rating, rCount) {
     let card = `
-      <div class="productCard">
+      <div data-id=${id} class="productCard">
               <img class="productImg" src=${image}>
               <h4>${title}</h4>
               <p>${des}</p>
--- details.js
+++ details.js
@@ -1,170 +1,27 @@
-let data = [
-    {
-        "id": 0,
-        "tittle": "UTESPELARE",
-        "des": "Gaming desk,160x80 cm(63x31 1/2)",
-        "cost": 11990,
-        "rating": 5,
-        "review": 10,
-        "hover_image": "https://www.ikea.com/in/en/images/products/utespelare-gaming-desk-black__0997777_pe822755_s5.jpg?f=m",
-        "first_image": "https://www.ikea.com/in/en/images/products/utespelare-gaming-desk-black__0985179_pe816538_s5.jpg?f=xl",
-        "second_image": "https://www.ikea.com/in/en/images/products/utespelare-gaming-desk-black__0997874_pe822843_s5.jpg?f=xl",
-        "third_image": "https://www.ikea.com/in/en/images/products/utespelare-gaming-desk-black__0997502_pe822689_s5.jpg?f=xl"
-    },
-    {
-        "id": 1,
-        "tittle": "MATCHSPEL",
-        "des": "Gaming chair",
-        "cost": 14990,
-        "rating": 4,
-        "review": 5,
-        "hover_image": "https://www.ikea.com/in/en/images/products/matchspel-gaming-chair-bomstad-white__0997855_pe822831_s5.jpg?f=s",
-        "first_image": "https://www.ikea.com/in/en/images/products/matchspel-gaming-chair-bomstad-white__0985646_pe816717_s5.jpg?f=s",
-        "second_image": "https://www.ikea.com/in/en/images/products/matchspel-gaming-chair-bomstad-white__0984833_pe816436_s5.jpg?f=s",
-        "third_image": "https://www.ikea.com/in/en/images/products/matchspel-gaming-chair-bomstad-white__0984829_pe816435_s5.jpg?f=s"
-    },
-    {
-        "id": 2,
-        "tittle": "UPPSPEL",
-        "des": "Table top cm(70 7/8)",
-        "cost": 8990,
-        "rating": 5,
-        "review": 30,
-        "hover_image": "https://www.ikea.com/in/en/images/products/uppspel-table-top-black__1007011_pe825940_s5.jpg?f=xxs",
-        "first_image": "https://www.ikea.com/in/en/images/products/uppspel-table-top-black__0984837_pe816441_s5.jpg?f=xxs",
-        "second_image": "https://www.ikea.com/in/en/images/products/uppspel-table-top-black__0997867_pe822842_s5.jpg?f=xxxs",
-        "third_image": "https://www.ikea.com/in/en/images/products/uppspel-table-top-black__0984838_pe816443_s5.jpg?f=xxxs"
-    },
-    {
-        "id": 3,
-        "tittle": "UPPSPEL",
-        "des": "pegboard, 76x56 cm (29 7/8x22)",
-        "cost": 1600,
-        "rating": 4,
-        "review": 5,
-        "hover_image": "https://www.ikea.com/in/en/images/products/uppspel-pegboard-black__0997548_pe822699_s5.jpg?f=xxxs",
-        "first_image": "https://www.ikea.com/in/en/images/products/uppspel-pegboard-black__0985137_pe816518_s5.jpg?f=xxxs",
-        "second_image": "https://www.ikea.com/in/en/images/products/uppspel-pegboard-black__0985138_pe816517_s5.jpg?f=xxxs",
-        "third_image": "https://www.ikea.com/in/en/images/products/uppspel-pegboard-black__1007009_pe825938_s5.jpg?f=xxxs"
-    },
-    {
-        "id": 4,
-        "tittle": "STYRSPEL",
-        "des": "Gaming chair",
-        "cost": 22990,
-        "rating": 4,
-        "review": 2,
-        "hover_image": "https://www.ikea.com/in/en/images/products/styrspel-gaming-chair-grey-red__1138897_pe880134_s5.jpg?f=xxxs",
-        "first_image": "https://www.ikea.com/in/en/images/products/styrspel-gaming-chair-grey-red__1115363_pe872045_s5.jpg?f=xxxs",
-        "second_image": "https://www.ikea.com/in/en/images/products/styrspel-gaming-chair-grey-red__1138896_pe880131_s5.jpg?f=xxxs",
-        "third_image": "https://www.ikea.com/in/en/images/products/styrspel-gaming-chair-grey-red__1138899_pe880132_s5.jpg?f=xxxs"
-    },
-    {
-        "id": 5,
-        "tittle": "UTESPELARE / HELMER",
-        "des": "Gaming chair",
-        "cost": 26970,
-        "rating": 4,
-        "review": 5,
-        "hover_image": "https://www.ikea.com/in/en/images/products/utespelare-helmer-desk-chair-and-drawer-unit-black__1007052_pe825949_s5.jpg?f=xxxs",
-        "first_image": "https://www.ikea.com/in/en/images/products/utespelare-helmer-desk-chair-and-drawer-unit-black__1004842_pe825226_s5.jpg?f=xxxs",
-        "second_image": "https://www.ikea.com/in/en/images/products/utespelare-helmer-desk-chair-and-drawer-unit-black__0997874_pe822843_s5.jpg?f=xxxs",
-        "third_image": "https://www.ikea.com/in/en/images/products/utespelare-helmer-desk-chair-and-drawer-unit-black__0984820_pe816427_s5.jpg?f=xxxs"
-    },
-    {
-        "id": 6,
-        "tittle": "LANESPELARE",
-        "des": "Accessories stand",
-        "cost": 1590,
-        "rating": 4,
-        "review": 1,
-        "hover_image": "https://www.ikea.com/in/en/images/products/lanespelare-accessories-stand__1083065_pe858960_s5.jpg?f=xxxs",
-        "first_image": "https://www.ikea.com/in/en/images/products/lanespelare-accessories-stand__0974868_pe812565_s5.jpg?f=xxxs",
-        "second_image": "https://www.ikea.com/in/en/images/products/lanespelare-accessories-stand__0984809_pe816415_s5.jpg?f=xxs",
-        "third_image": "https://www.ikea.com/in/en/images/products/lanespelare-accessories-stand__1046389_ph180841_s5.jpg?f=xxxs"
-    },
-    {
-        "id": 7,
-        "tittle": "HUVUDSPELARE",
-        "des": "Gaming chair",
-        "cost": 6990,
-        "rating": 4,
-        "review": 15,
-        "hover_image": "https://www.ikea.com/in/en/images/products/huvudspelare-gaming-chair-black__0961666_pe807698_s5.jpg?f=xxxs",
-        "first_image": "https://www.ikea.com/in/en/images/products/huvudspelare-gaming-chair-black__1039672_pe840417_s5.jpg?f=xxxs",
-        "second_image": "https://www.ikea.com/in/en/images/products/huvudspelare-gaming-chair-black__1046390_ph180886_s5.jpg?f=xxxs",
-        "third_image": "https://www.ikea.com/in/en/images/products/huvudspelare-gaming-chair-black__1039674_pe840418_s5.jpg?f=xxxs"
-    },
-    {
-        "id": 8,
-        "tittle": "UTESPELARE",
-        "des": "Gaming chair",
-        "cost": 10990,
-        "rating": 5,
-        "review": 5,
-        "hover_image": "https://www.ikea.com/in/en/images/products/utespelare-gaming-chair-bomstad-black__0997900_pe822868_s5.jpg?f=xxxs",
-        "first_image": "https://www.ikea.com/in/en/images/products/utespelare-gaming-chair-bomstad-black__0984819_pe816424_s5.jpg?f=xxxs",
-        "second_image": "https://www.ikea.com/in/en/images/products/utespelare-gaming-chair-bomstad-black__0985642_pe816712_s5.jpg?f=xxxs",
-        "third_image": "https://www.ikea.com/in/en/images/products/utespelare-gaming-chair-bomstad-black__0984822_pe816425_s5.jpg?f=xxxs"
-    },
-    {
-        "id": 9,
-        "tittle": "LANESPELARE",
-        "des": "Ring light with phone holder",
-        "cost": 3490,
-        "rating": 5,
-        "review": 3,
-        "hover_image": "https://www.ikea.com/in/en/images/products/lanespelare-ring-light-with-phone-holder__0971506_pe811367_s5.jpg?f=xxxs",
-        "first_image": "https://www.ikea.com/in/en/images/products/lanespelare-ring-light-with-phone-holder__0971505_pe811368_s5.jpg?f=xxxs",
-        "second_image": "https://www.ikea.com/in/en/images/products/lanespelare-ring-light-with-phone-holder__1046403_ph180816_s5.jpg?f=xxxs",
-        "third_image": "https://www.ikea.com/in/en/images/products/lanespelare-ring-light-with-phone-holder__1046390_ph180886_s5.jpg?f=xxxs"
-    },
-    {
-        "id": 10,
-        "tittle": "LANESPELARE",
-        "des": "Neck pillow",
-        "cost": 699,
-        "rating": 4,
-        "review": 2,
-        "hover_image": "https://www.ikea.com/in/en/images/products/lanespelare-neck-pillow__1000503_pe824289_s5.jpg?f=xxxs",
-        "first_image": "https://www.ikea.com/in/en/images/products/lanespelare-neck-pillow__0984936_pe816456_s5.jpg?f=xxxs",
-        "second_image": "https://www.ikea.com/in/en/images/products/lanespelare-neck-pillow__1046390_ph180886_s5.jpg?f=xxs",
-        "third_image": "https://www.ikea.com/in/en/images/products/lanespelare-neck-pillow__0984937_pe816455_s5.jpg?f=xxxs"
-    },
-    {
-        "id": 11,
-        "tittle": "FREDDE",
-        "des": "Gaming desk 140/185x74x146 cm (1/8/72 7/8x29 1/8x5 1/2)",
-        "cost": 19990,
-        "rating": 4,
-        "review": 12,
-        "hover_image": "https://www.ikea.com/in/en/images/products/fredde-gaming-desk-black__0403524_pe565583_s5.jpg?f=xxxs",
-        "first_image": "https://www.ikea.com/in/en/images/products/fredde-gaming-desk-black__0736008_pe740351_s5.jpg?f=xxxs",
-        "second_image": "https://www.ikea.com/in/en/images/products/fredde-gaming-desk-black__1191947_ph172928_s5.jpg?f=xxxs",
-        "third_image": "https://www.ikea.com/in/en/images/products/fredde-gaming-desk-black__1191946_ph172910_s5.jpg?f=xxxs"
-    }
-]
+let database = [];
+fetch("bookShelv.json")
+  .then((res) => res.json())
+  .then((data) => {
+    database = data;
+    load();
+  });
 
-load();
-
-let addtocartbtn = document.getElementById("addto-cart");
-
-addListener();
 
 function load() {
-    let id = +JSON.parse(localStorage.getItem("productid")) || 11
-    let product = data.find(x => x.id === id)
+  let id = JSON.parse(localStorage.getItem("productDetailId")) || 11
+  let product = database.find(x => +x.id === id)
 
 
-    let display = document.getElementById("container")
-    display.innerHTML = ` 
+  let display = document.getElementById("container")
+  display.innerHTML = ` 
     <div class="wrapper">
             <div class="leftdiv">
-                <img src="${product.first_image}"/>
-                <img src="${product.second_image}" />
-                <img src="${product.third_image}"/>
+                <img src="${product.image1}"/>
+                <img src="${product.image2}" />
+                <img src="${product.image3}"/>
+                <img src="${product.image4}"/>
                 <div class="desc">
-                <p>With this white 5-tier corner shelf, you can easily turn an unused corner into a smart storage space for your books and favourite things.</p>
+                <p>${product.description2}</p>
                 </div>
                <div> <p>Delivery and assembly prices not included.</p></div>
                 <div>Article number</div>
@@ -172,20 +29,20 @@ function load() {
                 <div class="more-info"><span>Product details</span><span><img src="./images/arrow.png"</span></div>
                 <div class="more-info"><span>Measurements</span><span><img src="./images/arrow.png"</span></div>
                 <div class="more-info"><div id="review-wrapper"><div class="review"><span>Reviews</span>
-                <img src="./images/stars.PNG"/></div><label class="stars-count">(${product.review})</label>
+                <img src="./images/stars.PNG"/></div><label class="stars-countb">(${product.rating[1]})</label>
                 </div><span><img src="./images/arrow.png"</span></div>
                 <div></div>
             </div> 
             <div class="rightdiv">
                 <div>
                     <div class="title">
-                        ${product.tittle}
+                        ${product.title}
                     </div>
-                    <span>${product.des}</span>
+                    <span>${product.description1}</span>
                 </div>
-                <p>Rs.<span> ${product.cost}</span></p>
+                <p>Rs.<span> ${product.price}</span></p>
                 <p>Price incl. of all taxes</p>
-                <p><img class="stars" src="./images/stars.PNG"/><label class="stars-count">(${product.review})</label></p>
+                <p><img class="stars" src="./images/stars.PNG"/><label class="stars-count">(${product.rating[1]})</label></p>
                 <p>How to get it</p>
                 <div class="cis">
                     <div> <img src="./images/store.PNG"/>
@@ -198,34 +55,41 @@ function load() {
                 </div>
                 <div><img data-id=${product.id} id="addto-cart" src="./images/a2c.png"/></div>
                 <div id="noti-div"> <label id="noti"></label></div>
-               
             </div>   
         </div>`
+  addListener();
 
+  let cartArr = JSON.parse(localStorage.getItem('cart')) || []
+  document.getElementById('count').innerText = cartArr.length;
 }
 
 
 function addListener() {
-    addtocartbtn.addEventListener('click', (e) => {
-        addToCartFn(e)
-    })
+  let addtocartbtn = document.getElementById("addto-cart");
+  addtocartbtn.addEventListener('click', (e) => {
+    addToCartFn(e)
+  })
 }
 
-let isItemAdded= false;
+let isItemAdded = false;
 function addToCartFn(e) {
-    if(isItemAdded) {
-        document.getElementById("noti").innerText = "Item already in cart"
-        return;
-    }
+  if (isItemAdded) {
+    document.getElementById("noti").innerText = "Item already in cart"
+    return;
+  }
 
-    let id = +e.target.dataset.id
-    let cartArr = JSON.parse(localStorage.getItem('cart')) || []
-    cartArr.push(id)
-    localStorage.setItem('cart', JSON.stringify(cartArr))
-    e.target.setAttribute("src", "./images/a2c2.png")
-    isItemAdded=true;
+  let id = +e.target.dataset.id
+  let cartArr = JSON.parse(localStorage.getItem('cart')) || []
+  cartArr.push(id)
+  localStorage.setItem('cart', JSON.stringify(cartArr))
+  e.target.setAttribute("src", "./images/a2c2.png")
+  isItemAdded = true;
+
+  document.getElementById('count').innerText = cartArr.length;
 }
 
-
+function cartClickFn() {
+  document.location.href ="shoppinglist.html"
+}
 
 
